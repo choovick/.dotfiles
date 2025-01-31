03b8f96fadfa8bfec7696f38742990f662ca0e08
Alexey Popovich <alexey.popovich@getinsured.com>
2025-01-30 19:59:00 -0500

    more mini!

--- stow/nvim/.config/nvim/lua/config/keymaps.lua
+++ stow/nvim/.config/nvim/lua/config/keymaps.lua
@@ -63,11 +63,26 @@ keymap.set("n", "<leader> ", function()
 end, { desc = "Fuzzy find files in cwd" })
 keymap.set("n", "<leader>ff", function()
   -- require("telescope.builtin").find_files({ cwd = vim.fn.expand("%:p:h"), follow = true })
-  require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h"), resume = false })
+  require("fzf-lua").files({
+    cwd = vim.fn.expand("%:p:h"),
+    resume = true
+  })
 end, { desc = "Fuzzy find files in current butter dir" })
+keymap.set("n", "<leader>fF", function()
+  -- require("telescope.builtin").find_files({ cwd = vim.fn.expand("%:p:h"), follow = true })
+  require("fzf-lua").files({
+    cwd = vim.fn.expand("%:p:h"),
+    resume = false
+  })
+end, { desc = "[New] Fuzzy find files in current butter dir" })
 
 keymap.set("n", "<leader>fr", function()
-  require("fzf-lua").oldfiles({ cwd_only = true, include_current_session = false, resume = false })
+  require("fzf-lua").oldfiles({
+    cwd_only = true,
+    -- show old files in current session as well
+    include_current_session = true,
+    resume = true
+  })
 end, { desc = "Old files in current dir" })
 keymap.set("n", "<leader>fR", function()
   require("fzf-lua").oldfiles()
@@ -83,7 +98,10 @@ keymap.set(
   { desc = "Live grep with rg --glob support" }
 )
 keymap.set("n", "<leader>fd", function()
-  require("fzf-lua").live_grep_glob({ cwd = vim.fn.expand("%:p:h"), resume = true })
+  require("fzf-lua").live_grep_glob({
+    cwd = vim.fn.expand("%:p:h"),
+    resume = true
+  })
 end, { desc = "Live grep in current buffer directory" })
 keymap.set("n", "<leader>fc", "<cmd>FzfLua grep_cword<cr>", { desc = "Find string under cursor in cwd" })
 keymap.set("n", "<leader>fl", "<cmd>FzfLua lgrep_curbuf resume=true<cr>", { desc = "Live grep in current buffer" })
