bfc1437d419bbe9c262ddf7da23b73f3457ce2b9
Alexey Popovich <alexey.popovich@getinsured.com>
2025-01-31 15:04:34 -0500

    adding kcl custom icons, tweaking inactive pane color to see current line better

---
+++ stow/nvim/.config/nvim/lua/plugins/nvim-web-devicons.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-tree/nvim-web-devicons",
+  opts = {
+    override_by_extension = {
+      ["k"] = {
+        icon = "󰰉",
+        color = "#81e043",
+        name = "KCL",
+      },
+      ["mod"] = {
+        icon = "󰰋",
+        color = "#81e043",
+        name = "KCL",
+      },
+    },
+  },
+}
