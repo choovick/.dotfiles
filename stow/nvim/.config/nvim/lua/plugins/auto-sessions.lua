6a1fff920f4edc9f2908311bccc33c274a46ca71
Alexey Popovich <alexey.popovich@getinsured.com>
2025-02-01 10:17:46 -0500

    No auto stuff... annoying

--- stow/nvim/.config/nvim/lua/plugins/auto-sessions.lua
+++ stow/nvim/.config/nvim/lua/plugins/auto-sessions.lua
@@ -6,15 +6,15 @@ return {
     local auto_session = require("auto-session")
 
     auto_session.setup({
-      enabled = true, -- Enables/disables auto creating, saving and restoring
+      enabled = false, -- Enables/disables auto creating, saving and restoring
       root_dir = vim.fn.stdpath("data") .. "/sessions/", -- Root dir where sessions will be stored
       auto_save = true, -- Enables/disables auto saving session on exit
-      auto_restore = true, -- Enables/disables auto restoring session on start
+      auto_restore = false, -- Enables/disables auto restoring session on start
       auto_create = true, -- Enables/disables auto creating new session files. Can take a function that should return true/false if a new session file should be created or not
       suppressed_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" }, -- Suppress session restore/create in certain directories
       allowed_dirs = nil, -- Allow session restore/create in certain directories
 
-      auto_restore_last_session = true, -- On startup, loads the last saved session if session for cwd does not exist
+      auto_restore_last_session = false, -- On startup, loads the last saved session if session for cwd does not exist
 
       use_git_branch = false, -- Include git branch name in session name
       lazy_support = true, -- Automatically detect if Lazy.nvim is being used and wait until Lazy is done to make sure session is restored correctly. Does nothing if Lazy isn't being used. Can be disabled if a problem is suspected or for debugging
@@ -29,7 +29,7 @@ return {
 
       pre_save_cmds = {
         -- Close NvimTree before saving session
-        "NvimTreeClose",
+        -- "NvimTreeClose",
       },
 
       post_restore_cmds = {
