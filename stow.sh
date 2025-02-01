d3126901acc8d18085b727821f29feb2ec1df1ed
Alexey Popovich <alexey.popovich@getinsured.com>
2025-02-01 14:04:27 -0500

    format

--- stow.sh
+++ stow.sh
@@ -9,8 +9,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # running stow for each directory in the stow directory
 for dir in "$SCRIPT_DIR/stow"/*; do
-    if [ -d "$dir" ]; then
-        echo "Running command: stow -v -d \"$SCRIPT_DIR/stow\" -t \"${HOME}\" \"$(basename "$dir")\""
-        stow -v -d "$SCRIPT_DIR/stow" -t "${HOME}" "$(basename "$dir")"
-    fi
+  if [ -d "$dir" ]; then
+    echo "Running command: stow -v -d \"$SCRIPT_DIR/stow\" -t \"${HOME}\" \"$(basename "$dir")\""
+    stow -v -d "$SCRIPT_DIR/stow" -t "${HOME}" "$(basename "$dir")"
+  fi
 done
