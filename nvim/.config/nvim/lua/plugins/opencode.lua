4e4817ade3c0ae2cacd3d2c4baa340712e9dac3d
Alexey Popovich <alexey.popovich@getinsured.com>
2026-02-14 20:43:32 -0500

    align opencode keymaps with current api
    Use the supported ask/select/session commands and add explicit server/session selectors so prompts reliably target the intended running session.

--- nvim/.config/nvim/lua/plugins/opencode.lua
+++ nvim/.config/nvim/lua/plugins/opencode.lua
@@ -10,12 +10,14 @@ return {
   -- stylua: ignore
   keys = {
     { '<leader>oo', function() require('opencode').toggle() end, desc = 'Toggle embedded opencode', },
-    { '<leader>oa', function() require('opencode').ask('@cursor: ', {}) end, desc = 'Ask opencode', mode = 'n', },
-    { '<leader>oa', function() require('opencode').ask('@selection: ', {}) end, desc = 'Ask opencode about selection', mode = 'v', },
-    { '<leader>op', function() require('opencode').select_prompt() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
-    { '<leader>on', function() require('opencode').command('session_new') end, desc = 'New session', },
+    { '<leader>oa', function() require('opencode').ask('@this: ', { submit = true }) end, desc = 'Ask opencode', mode = 'n', },
+    { '<leader>oa', function() require('opencode').ask('@this: ', { submit = true }) end, desc = 'Ask opencode about selection', mode = 'v', },
+    { '<leader>op', function() require('opencode').select() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
+    { '<leader>on', function() require('opencode').command('session.new') end, desc = 'New session', },
+    { '<leader>os', function() require('opencode').select_server() end, desc = 'Select server', },
+    { '<leader>oS', function() require('opencode').select_session() end, desc = 'Select session', },
     { '<leader>oy', function() require('opencode').command('messages_copy') end, desc = 'Copy last message', },
-    { '<M-u>', function() require('opencode').command('messages_half_page_up') end, desc = 'Scroll messages up', mode = { 'n', 'i', 't' }, },
-    { '<M-d>', function() require('opencode').command('messages_half_page_down') end, desc = 'Scroll messages down', mode = { 'n', 'i', 't' }, },
+    { '<M-u>', function() require('opencode').command('session.half.page.up') end, desc = 'Scroll messages up', mode = { 'n', 'i', 't' }, },
+    { '<M-d>', function() require('opencode').command('session.half.page.down') end, desc = 'Scroll messages down', mode = { 'n', 'i', 't' }, },
   },
 }
