d6f77f226e50810c26bad74bda89e6635b714c1b
Alexey Popovich <alexey.popovich@getinsured.com>
2026-04-09 10:26:24 -0400

    copilot tweaks

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -3,14 +3,17 @@ return {
   -- enabled = false,
   cmd = "Copilot",
   event = "InsertEnter",
-  build = ":Copilot auth",
+  -- Run `:Copilot auth` once after install (see plugin README); avoid `build` so updates do not re-trigger auth.
+  -- Full default `setup()` options: https://github.com/zbirenbaum/copilot.lua#setup-and-configuration
   opts = {
-    -- for lua-cmp
+    panel = { enabled = false },
+    -- Inline ghost text; hide when blink.cmp menu is open (User autocommands from plugin README).
     suggestion = {
       enabled = true,
       auto_trigger = true,
       hide_during_completion = true,
       debounce = 75,
+      trigger_on_accept = true,
       keymap = {
         accept = "<M-l>",
         accept_word = "<M-j>",
@@ -20,7 +23,6 @@ return {
         dismiss = "<M-h>",
       },
     },
-    -- panel = { enabled = false },
     -- filetypes
     -- https://github.com/zbirenbaum/copilot.lua?tab=readme-ov-file#filetypes
     filetypes = {
@@ -36,7 +38,25 @@ return {
       end,
     },
   },
-  -- hotkeys lazy
+  config = function(_, opts)
+    require("copilot").setup(opts)
+
+    local group = vim.api.nvim_create_augroup("CopilotBlinkCmp", { clear = true })
+    vim.api.nvim_create_autocmd("User", {
+      group = group,
+      pattern = "BlinkCmpMenuOpen",
+      callback = function()
+        vim.b.copilot_suggestion_hidden = true
+      end,
+    })
+    vim.api.nvim_create_autocmd("User", {
+      group = group,
+      pattern = "BlinkCmpMenuClose",
+      callback = function()
+        vim.b.copilot_suggestion_hidden = false
+      end,
+    })
+  end,
   keys = {
     -- leader o r to disable/enable copilot in the single hotkey
     { "<leader>or", "<cmd>Copilot disable<CR><cmd>Copilot enable<CR>", desc = "Copilot Disable/Enable" },
