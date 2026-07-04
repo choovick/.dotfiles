0915cb41c5a30a69abc21da287da932bb2b6e260
Alexey Popovich <alexey.popovich@getinsured.com>
2026-07-04 10:57:04 -0400

    managing treesitter languages

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,36 @@
+local parsers = {
+  "bash",
+  "zsh",
+  "yaml",
+  "terraform",
+  "graphql",
+  "helm",
+  "python",
+  "javascript",
+  "html",
+  "json",
+}
+
+local function install_parsers()
+  require("nvim-treesitter").install(parsers)
+end
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
+    dependencies = { "williamboman/mason.nvim" },
     lazy = false,
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter").setup()
+
+      install_parsers()
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MasonToolsUpdateCompleted",
+        callback = install_parsers,
+      })
     end,
   },
   {
