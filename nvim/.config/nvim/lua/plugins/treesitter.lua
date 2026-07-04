ad781d1b266379eae0ce9c6db4dcdd25c33f6562
Alexey Popovich <alexey.popovich@getinsured.com>
2026-07-04 11:41:38 -0400

    fixing tfvars highlight

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -11,6 +11,9 @@ local parsers = {
   "json",
 }
 
+-- Filetypes that use a treesitter parser but are not named after it.
+local highlight_filetypes = vim.list_extend({ "terraform-vars" }, parsers)
+
 local function install_parsers()
   require("nvim-treesitter").install(parsers)
 end
@@ -26,6 +29,24 @@ return {
       require("nvim-treesitter").setup()
 
       install_parsers()
+
+      -- Neovim 0.12: treesitter highlighting is opt-in per filetype.
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = highlight_filetypes,
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
+
+      -- No runtime syntax/terraform-vars.vim; reuse HCL rules as fallback.
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "terraform-vars",
+        callback = function()
+          if vim.fn.exists("b:current_syntax") == 0 then
+            vim.cmd.runtime("syntax/terraform.vim")
+          end
+        end,
+      })
 
       vim.api.nvim_create_autocmd("User", {
         pattern = "MasonToolsUpdateCompleted",
