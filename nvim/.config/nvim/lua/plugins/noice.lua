389faaec24fe17749b45de2c51d24a1c53f23cf4
Alexey Popovich <alexey.popovich@getinsured.com>
2026-07-04 19:35:59 -0400

    fix notification

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -35,6 +35,14 @@ return {
   },
   dependencies = {
     "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
+    {
+      "rcarriga/nvim-notify",
+      config = function()
+        -- gruvbox transparent_mode leaves NotifyBackground without bg
+        require("notify").setup({
+          background_colour = "#000000",
+        })
+      end,
+    },
   },
 }
