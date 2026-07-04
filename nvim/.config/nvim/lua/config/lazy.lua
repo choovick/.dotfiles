bc6ea7154db452cdbb493f3cbdac5623575e9ce6
Alexey Popovich <alexey.popovich@getinsured.com>
2026-07-04 19:02:56 -0400

    theme

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "gruvbox" } },
   -- automatically check for plugin updates
   change_detection = {
     notify = false,
