f6d06dfab4cc1909b108d03ca5d85260a20dc0a0
Alexey Popovich <alexey.popovich@getinsured.com>
2026-07-04 10:36:28 -0400

    fixing comments config

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,10 +1,12 @@
 return {
   "numToStr/Comment.nvim",
   config = function()
-    require("Comment").setup()
-
     local ft = require("Comment.ft")
     -- adding kcl
     ft.set("kcl", { "# %s", '"""%s\n"""' })
+    -- .zshrc-func and other zsh buffers use filetype=zsh, which Comment.nvim
+    -- does not ship by default (only bash/sh).
+    ft.set("zsh", ft.get("bash"))
+
   end,
 }
